8ea621c5f4460c3859f4c5fc19c9f855d58c3ea3
Usuario <Usuario@DESKTOP-3AJ59NG>
2022-07-02 21:51:09 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleAppSimuladorDeYagoTorres/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppSimuladorDeYagoTorres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese una hora");
+            var hora = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese los minutos");
+            var minutos = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese los segundos");
+            var segundos = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el formato que desea obtener AM ó PM");
+            var FormatoSelect = Convert.ToString(Console.ReadLine());
+            if (FormatoSelect == "AM" || FormatoSelect == "am")
+            {
+                Verificar(hora, minutos, segundos);
+                {
+                    Console.WriteLine($"{hora}:{minutos}:{segundos} {FormatoSelect}");
+                    Console.WriteLine($"{hora}:{minutos}:{segundos} FORMATO EN 24hs");
+                }
+
+            }
+            else if (FormatoSelect == "PM" || FormatoSelect == "pm")
+            { 
+                if (Verificar(hora, minutos, segundos)) ;
+                {
+                    Console.WriteLine($"{hora}:{minutos}:{segundos} {FormatoSelect}");
+                    hora = hora + 12;
+                    Console.WriteLine($"{hora}:{minutos}:{segundos} FORMATO EN 24hs");
+                }
+            }
+            Console.ReadLine();
+
+        }
+
+        private static bool Verificar(int hora, int minutos, int segundos)
+        {
+            if (hora > 0 && hora <= 12 || minutos > 0 && minutos <= 59 || segundos > 0 && segundos <= 59)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
